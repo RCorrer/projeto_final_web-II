ba478a0a8829aa2176a8ce4edce69845eacf2733
lvtrindade <lucasventurintrindade@gmail.com>
2025-03-28 18:38:10 -0300

    feat(elements): Estilização login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,23 @@
 import { Routes } from '@angular/router';
 import { OrcamentoFuncionarioComponent } from './components/orcamento-funcionario/orcamento-funcionario.component';
 import { TelaInicialFuncionarioComponent } from './components/tela-inicial-funcionario/tela-inicial-funcionario.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 export const routes: Routes = [
-    {
-        path: 'efetuar-orcamento', component: OrcamentoFuncionarioComponent
-    },
-    {
-      path: 'home', component: TelaInicialFuncionarioComponent
-    }
+
+  {
+    path: '',
+    component: LoginComponent
+  },
+
+  {
+    path: 'efetuar-orcamento',
+    component: OrcamentoFuncionarioComponent
+  },
+    
+  {
+    path: 'home',
+    component: TelaInicialFuncionarioComponent  
+  }
 ];
