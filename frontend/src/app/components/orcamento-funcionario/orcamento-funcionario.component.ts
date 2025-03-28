3cf2f0f183af6f54bf6888f0fb7ac4424d94ac00
Pedro Lemos <p.a.lemos1@gmail.com>
2025-03-27 21:52:27 -0300

    criando componente orcamento

---
+++ frontend/src/app/components/orcamento-funcionario/orcamento-funcionario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orcamento-funcionario',
+  imports: [],
+  templateUrl: './orcamento-funcionario.component.html',
+  styleUrl: './orcamento-funcionario.component.css'
+})
+export class OrcamentoFuncionarioComponent {
+
+}
