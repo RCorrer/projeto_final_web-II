9b72dc34dfe32c6f9c01502f5414d2736a48958f
Pedro Lemos <p.a.lemos1@gmail.com>
2025-03-28 10:08:45 -0300

    Botao enviar
    Adicionado botao "Enviar" (sem logica, so com um console log pra teste)
    E algumas correcoes de identacao

--- frontend/src/app/components/orcamento-funcionario/orcamento-funcionario.component.ts
+++ frontend/src/app/components/orcamento-funcionario/orcamento-funcionario.component.ts
@@ -9,6 +9,6 @@ import { Component } from '@angular/core';
 export class OrcamentoFuncionarioComponent {
   enviarOrcamento() {
     console.log('BUM! ENVIADO (mentira, só depois de criar um serviço aqui)');
-  
+
   }
 }
