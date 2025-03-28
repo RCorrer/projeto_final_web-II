8e319dfe51823c7ed860d82b4171ee7dfe332a6c
joaovitorliskoskiwalter <joaowalter193@gmail.com>
2025-03-28 11:28:12 -0300

    Routes consertado

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { OrcamentoFuncionarioComponent } from './components/orcamento-funcionario/orcamento-funcionario.component';
+import { TelaInicialFuncionarioComponent } from './components/tela-inicial-funcionario/tela-inicial-funcionario.component';
+
 
 export const routes: Routes = [
     {
         path: 'efetuar-orcamento', component: OrcamentoFuncionarioComponent
     },
-import { TelaInicialFuncionarioComponent } from './components/tela-inicial-funcionario/tela-inicial-funcionario.component';
-
-export const routes: Routes = [
-    { path: 'home', component: TelaInicialFuncionarioComponent },
+    {
+      path: 'home', component: TelaInicialFuncionarioComponent
+    }
 ];
