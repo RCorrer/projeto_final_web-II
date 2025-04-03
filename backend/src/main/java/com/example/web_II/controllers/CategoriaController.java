965910b58a296fbd207fbb4b777713b2bfec53ad
joaovitorliskoskiwalter <joaowalter193@gmail.com>
2025-04-03 11:20:34 -0300

    feat(endpoint) : Tratamento de erro para endpoint Categoria

--- backend/src/main/java/com/example/web_II/controllers/CategoriaController.java
+++ backend/src/main/java/com/example/web_II/controllers/CategoriaController.java
@@ -6,6 +6,8 @@ import com.example.web_II.domain.categoria.CategoriaDTO;
 import com.example.web_II.repositories.CategoriaRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,8 +23,10 @@ private CategoriaRepository categoriaRepository;
 @PostMapping("/categoria")
 public ResponseEntity addCategoria(@RequestBody @Valid CategoriaDTO data){
 
+    if (categoriaRepository.existsByDescricao(data.descricao())){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("Essa categoria já existe");
+    }
     Categoria novaCategoria = new Categoria(data.descricao());
-
     this.categoriaRepository.save(novaCategoria);
     return ResponseEntity.ok("Categoria Adicionada!!");
 }
--- backend/src/main/java/com/example/web_II/repositories/CategoriaRepository.java
+++ backend/src/main/java/com/example/web_II/repositories/CategoriaRepository.java
@@ -4,4 +4,5 @@ import com.example.web_II.domain.categoria.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, String> {
+    boolean existsByDescricao(String descricao);
 }
