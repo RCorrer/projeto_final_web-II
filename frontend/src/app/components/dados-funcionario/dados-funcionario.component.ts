ac522320ef38de7fe09462d670fa95aa33c8ee33
Sophia Costa <aishspo@gmail.com>
2025-04-04 17:53:25 -0300

    feat(RF018): substituir dialogs antigos por novos componentes

---
+++ frontend/src/app/components/dados-funcionario/dados-funcionario.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { materialImports } from '../../material-imports';
+
+@Component({
+  selector: 'app-dados-funcionario',
+  imports: [...materialImports],
+  templateUrl: './dados-funcionario.component.html',
+  styleUrl: './dados-funcionario.component.css'
+})
+export class DadosFuncionarioComponent {
+
+}
