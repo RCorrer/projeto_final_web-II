ce4d9fcc8773177d9d338675d20f5f83d660bac9
Sophia Costa <aishspo@gmail.com>
2025-04-04 18:01:42 -0300

    feat: atualizar arquivo com novas rotas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,23 +1,33 @@
-import { Routes } from '@angular/router';
-import { OrcamentoFuncionarioComponent } from './components/orcamento-funcionario/orcamento-funcionario.component';
-import { TelaInicialFuncionarioComponent } from './components/tela-inicial-funcionario/tela-inicial-funcionario.component';
-import { LoginComponent } from './components/login/login.component';
-
+import { Routes } from "@angular/router";
+import { OrcamentoFuncionarioComponent } from "./components/orcamento-funcionario/orcamento-funcionario.component";
+import { TelaInicialFuncionarioComponent } from "./components/tela-inicial-funcionario/tela-inicial-funcionario.component";
+import { LoginComponent } from "./components/login/login.component";
+import { CrudFuncionariosComponent } from "./components/crud-funcionarios/crud-funcionarios.component";
+import { CrudEquipamentosComponent } from "./components/crud-equipamentos/crud-equipamentos.component";
 
 export const routes: Routes = [
-
   {
-    path: '',
-    component: LoginComponent
+    path: "",
+    component: LoginComponent,
   },
 
   {
-    path: 'efetuar-orcamento',
-    component: OrcamentoFuncionarioComponent
+    path: "efetuar-orcamento",
+    component: OrcamentoFuncionarioComponent,
   },
-    
+
   {
-    path: 'home',
-    component: TelaInicialFuncionarioComponent  
-  }
+    path: "home",
+    component: TelaInicialFuncionarioComponent,
+  },
+
+  {
+    path: "funcionarios",
+    component: CrudFuncionariosComponent,
+  },
+
+  {
+    path: "equipamentos",
+    component: CrudEquipamentosComponent,
+  },
 ];
