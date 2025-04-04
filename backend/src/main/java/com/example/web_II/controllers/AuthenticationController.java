b6eb9a80581cfeb42857ceed251b64b8bc45cb2e
Rafael Correr <119776059+WindFox8@users.noreply.github.com>
2025-04-04 19:28:25 -0300

    refactor(endpoint): separando o service do endpoint

--- backend/src/main/java/com/example/web_II/controllers/AuthenticationController.java
+++ backend/src/main/java/com/example/web_II/controllers/AuthenticationController.java
@@ -1,25 +1,12 @@
 package com.example.web_II.controllers;
 
 import com.example.web_II.domain.cliente.CadastroClienteDTO;
-import com.example.web_II.domain.cliente.Cliente;
-import com.example.web_II.domain.enderecos.Endereco;
-import com.example.web_II.domain.funcionarios.Funcionario;
 import com.example.web_II.domain.usuarios.AuthenticationDTO;
-import com.example.web_II.domain.usuarios.LoginResponseDTO;
 import com.example.web_II.domain.funcionarios.CadastroFuncionarioDTO;
-import com.example.web_II.domain.usuarios.Usuario;
-import com.example.web_II.domain.usuarios.UsuarioRole;
-import com.example.web_II.infra.security.TokenService;
-import com.example.web_II.repositories.ClienteRepository;
-import com.example.web_II.repositories.EnderecoRepository;
-import com.example.web_II.repositories.FuncionarioRepository;
-import com.example.web_II.repositories.UsuarioRepository;
+import com.example.web_II.services.AuthService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,68 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthenticationController {
     @Autowired
-    private AuthenticationManager authenticationManager;
-    @Autowired
-    private UsuarioRepository usuarioRepository;
-    @Autowired
-    private FuncionarioRepository funcionarioRepository;
-    @Autowired
-    private EnderecoRepository enderecoRepository;
-    @Autowired
-    private ClienteRepository clienteRepository;
-    @Autowired
-    private TokenService tokenService;
+    private AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data){
-        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
-        var auth = this.authenticationManager.authenticate(usernamePassword);
-
-        var token = tokenService.generateToken((Usuario) auth.getPrincipal());
-
-        var userDetails = this.usuarioRepository.findByEmail(data.login());
-
-        return ResponseEntity.ok(new LoginResponseDTO(
-                token,
-                userDetails.getNome(),
-                userDetails.getId(),
-                userDetails.getRole().toString(),
-                "Login efetuado com sucesso"
-        ));
+    public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data) {
+        var response = authService.authenticate(data);
+        return ResponseEntity.ok(response);
     }
 
     @Transactional
     @PostMapping("/cadastro/funcionario")
-    public ResponseEntity register(@RequestBody @Valid CadastroFuncionarioDTO data){
-        if(this.usuarioRepository.findUserDetailsByEmail(data.login()) != null) return ResponseEntity.badRequest().build();
-
-        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
-        Usuario novoUsuario = new Usuario(data.name(), data.login(), encryptedPassword, UsuarioRole.FUNCIONARIO);
-
-        Funcionario novoFuncionario = new Funcionario(data.dataNascimento(), novoUsuario);
-
-        this.usuarioRepository.save(novoUsuario);
-        this.funcionarioRepository.save(novoFuncionario);
-
-        return ResponseEntity.ok("Funcionário cadastrado com sucesso");
+    public ResponseEntity registerFuncionario(@RequestBody @Valid CadastroFuncionarioDTO data) {
+        return authService.registerFuncionario(data);
     }
 
     @Transactional
     @PostMapping("/cadastro/cliente")
-    public ResponseEntity register(@RequestBody @Valid CadastroClienteDTO data){
-        if(this.usuarioRepository.findUserDetailsByEmail(data.login()) != null) return ResponseEntity.badRequest().build();
-
-        String encryptedPassword = new BCryptPasswordEncoder().encode(Cliente.gerarSenha());
-        Usuario novoUsuario = new Usuario(data.name(), data.login(), encryptedPassword, UsuarioRole.CLIENTE);
-
-        Endereco novoEndereco = new Endereco(data.cep(), data.logradouro(), data.complemento(), data.unidade(), data.bairro(), data.localidade(), data.uf(), data.estado(), data.regiao(), data.numero());
-
-        Cliente novoCliente = new Cliente(data.cpf(), data.telefone(), novoEndereco, novoUsuario);
-
-        this.enderecoRepository.save(novoEndereco);
-        this.usuarioRepository.save(novoUsuario);
-        this.clienteRepository.save(novoCliente);
-
-        return ResponseEntity.ok("Usuário cadastrado com sucesso");
+    public ResponseEntity registerCliente(@RequestBody @Valid CadastroClienteDTO data) {
+        return authService.registerCliente(data);
     }
 }
+
