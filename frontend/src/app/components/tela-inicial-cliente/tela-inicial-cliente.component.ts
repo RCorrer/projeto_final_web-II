7f1bdc5fbdd6ebbde53a5b5935bbb714b47f1983
Pedro Lemos <p.a.lemos1@gmail.com>
2025-04-10 08:17:35 -0300

    feat/style(RF012)
    Adicionar metodo para enviar orcamento e registrar data/hora.
    
    Remover dados fixos do HTML e adicionar binding e dados mockados no TS para teste.
    
    Padronizar estilo.

--- frontend/src/app/components/tela-inicial-cliente/tela-inicial-cliente.component.ts
+++ frontend/src/app/components/tela-inicial-cliente/tela-inicial-cliente.component.ts
@@ -15,14 +15,14 @@ import { NavbarComponent } from '../navbar/navbar.component';
 export class TelaInicialClienteComponent {
 
   solicitacoes = [
-    { id: 1, dataHora: '2025-04-01 14:30', equipamento: 'Notebook Dell Inspiron', estado: 'ABERTA' },
-    { id: 2, dataHora: '2025-04-02 15:15', equipamento: 'Impressora HP LaserJet', estado: 'ORÇADA' },
-    { id: 3, dataHora: '2025-04-03 16:45', equipamento: 'Monitor LG 24”', estado: 'REJEITADA' },
-    { id: 4, dataHora: '2025-04-04 17:20', equipamento: 'Teclado Mecânico Redragon', estado: 'APROVADA' },
-    { id: 5, dataHora: '2025-04-05 08:30', equipamento: 'Notebook Dell Inspiron', estado: 'REDIRECIONADA' },
-    { id: 6, dataHora: '2025-04-06 09:15', equipamento: 'Impressora HP LaserJet', estado: 'ARRUMADA' },
-    { id: 7, dataHora: '2025-04-07 10:45', equipamento: 'Monitor LG 24”', estado: 'PAGA' },
-    { id: 8, dataHora: '2025-04-08 11:20', equipamento: 'Teclado Mecânico Redragon', estado: 'FINALIZADA' }
+    { id: 1, dataHora: '2025-04-01 14:30', equipamento: 'Notebook Dell', estado: 'ABERTA' },
+    { id: 2, dataHora: '2025-04-02 15:15', equipamento: 'Impressora Xerox', estado: 'ORÇADA' },
+    { id: 3, dataHora: '2025-04-03 16:45', equipamento: 'Mouse de bolinha', estado: 'REJEITADA' },
+    { id: 4, dataHora: '2025-04-04 17:20', equipamento: 'Teclado com farelo de pão', estado: 'APROVADA' },
+    { id: 5, dataHora: '2025-04-05 08:30', equipamento: 'Notebook Positivo feito pra defeito', estado: 'REDIRECIONADA' },
+    { id: 6, dataHora: '2025-04-06 09:15', equipamento: 'Impressora HP (HarryPotter)', estado: 'ARRUMADA' },
+    { id: 7, dataHora: '2025-04-07 10:45', equipamento: 'Monitor Tubo', estado: 'PAGA' },
+    { id: 8, dataHora: '2025-04-08 11:20', equipamento: 'Monitor LG 24"', estado: 'FINALIZADA' }
   ];
 
   visualizarSolicitacao(id: number) {
