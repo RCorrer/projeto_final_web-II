05e4aa81e2685d42066b714214ca48fc3cd83081
lvtrindade <lucasventurintrindade@gmail.com>
2025-04-11 16:04:22 -0300

    feat(elements): criação do componente para solicitar manutencao.

---
+++ frontend/src/app/tela-solicitar-manutencao/tela-solicitar-manutencao.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-solicitar-manutencao',
+  imports: [],
+  templateUrl: './tela-solicitar-manutencao.component.html',
+  styleUrl: './tela-solicitar-manutencao.component.css'
+})
+export class TelaSolicitarManutencaoComponent {
+
+}
