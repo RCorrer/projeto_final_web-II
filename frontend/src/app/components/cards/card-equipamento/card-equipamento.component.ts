9c0cf665fe82ff4b5a113127e2e97f9357e2df39
Sophia Costa <aishspo@gmail.com>
2025-04-11 16:29:20 -0300

    refactor: tela equipamentos

--- frontend/src/app/components/cards/card-equipamento/card-equipamento.component.ts
+++ frontend/src/app/components/cards/card-equipamento/card-equipamento.component.ts
@@ -1,8 +1,10 @@
-import { Component, inject } from "@angular/core";
+import { Component, inject, Input } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { materialImports } from "../../../material-imports";
 import { MatDialog } from "@angular/material/dialog";
 import { DadosEquipamentoComponent } from "../../dados-equipamento/dados-equipamento.component";
+import { CardBaseComponent, MenuItem } from "../card-base/card-base.component";
+import { Equipamento } from "../../../models/equipamento.model";
 
 @Component({
   selector: "app-card-equipamento",
@@ -11,24 +13,28 @@ import { DadosEquipamentoComponent } from "../../dados-equipamento/dados-equipam
   templateUrl: "./card-equipamento.component.html",
   styleUrl: "./card-equipamento.component.css",
 })
-export class CardEquipamentoComponent {
-  equipamentos = [
-    { id: 1, categoria: "Computador" },
-    { id: 2, categoria: "Periférico" },
-    { id: 3, categoria: "Monitor" },
-    { id: 4, categoria: "Rede" },
-  ];
+export class CardEquipamentoComponent extends CardBaseComponent<Equipamento> {
+  @Input() equipamento!: Equipamento;
 
-  equipamentoSelecionado: any;
+  override getMenuItems(): MenuItem[] {
+    return [
+      {
+        icon: "edit",
+        label: "Editar",
+        action: () => this.editar.emit(this.equipamento),
+      },
+      {
+        icon: "delete",
+        label: "Excluir",
+        action: () => this.excluir.emit(this.equipamento),
+      },
+    ];
+  }
   private dialog = inject(MatDialog);
 
   abrirDialog() {
     this.dialog.open(DadosEquipamentoComponent, {
       width: "600px",
     });
-  }
-
-  excluir(equipamento: any) {
-    console.log(`Excluindo: ${equipamento.categoria}`);
   }
 }
--- frontend/src/app/pages/tela-equipamentos/tela-equipamentos.component.ts
+++ frontend/src/app/pages/tela-equipamentos/tela-equipamentos.component.ts
@@ -5,14 +5,29 @@ import { MatDialog } from "@angular/material/dialog";
 
 import { CardEquipamentoComponent } from "../../components/cards/card-equipamento/card-equipamento.component";
 import { DadosEquipamentoComponent } from "../../components/dados-equipamento/dados-equipamento.component";
+import { Equipamento } from "../../models/equipamento.model";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-crud-equipamentos",
-  imports: [...materialImports, NavbarComponent, CardEquipamentoComponent],
-  templateUrl: "./crud-equipamentos.component.html",
-  styleUrl: "./crud-equipamentos.component.css",
+  imports: [
+    ...materialImports,
+    NavbarComponent,
+    CardEquipamentoComponent,
+    CommonModule,
+  ],
+  templateUrl: "./tela-equipamentos.component.html",
+  styleUrl: "./tela-equipamentos.component.css",
 })
-export class CrudEquipamentosComponent {
+export class TelaEquipamentosComponent {
+  equipamentos: Equipamento[] = [
+    { descricao: "Computador" },
+    { descricao: "Periférico" },
+    { descricao: "Monitor" },
+    { descricao: "Rede" },
+    { descricao: "Impressora" },
+  ];
+
   private dialog = inject(MatDialog);
   abrirDialog() {
     this.dialog.open(DadosEquipamentoComponent, {
