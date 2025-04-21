6c045fb30a6d81f4aabc5c3ac71e76ae5bc6d703
Pedro Lemos <p.a.lemos1@gmail.com>
2025-04-21 14:05:57 -0300

    feat: solicitacoes no dashboard do funcionario
    Adicionado dados mockados para receber os cards de solicitacao na tela inicial do funcionario.
    
    feito ngFor para receber a lista de diferentes solicitacoes ao inves dos dados estaticos no html

--- frontend/src/app/components/cards/card-solicitacao/card-solicitacao.component.ts
+++ frontend/src/app/components/cards/card-solicitacao/card-solicitacao.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { materialImports } from '../../../material-imports';
 import { CommonModule } from '@angular/common';
+import { Solicitacao } from '../../../models/Solicitacao.model';
 
 @Component({
   selector: 'app-card-solicitacao',
@@ -8,6 +9,37 @@ import { CommonModule } from '@angular/common';
   templateUrl: './card-solicitacao.component.html',
   styleUrl: './card-solicitacao.component.css'
 })
+
 export class CardSolicitacaoComponent {
+  @Input() solicitacao!: Solicitacao;
+
   backgroundColor: string = 'var(--color-aberto)';
+
+  ngOnInit(): void {
+    this.backgroundColor = this.getColor (this.solicitacao.estado);
+  }
+
+  getColor(estado: string): string {
+    switch (estado) {
+      case 'ABERTA':
+        return 'var(--color-aberto)';
+      case 'ORCADA':
+        return 'var(--color-orcada)';
+      case 'REJEITADA':
+        return 'var(--color-rejeitada)';
+      case 'APROVADA':
+        return 'var(--color-aprovada)';
+      case 'REDIRECIONADA':
+        return 'var(--color-redirecionada)';
+      case 'ARRUMADA':
+        return 'var(--color-arrumada)';
+      case 'PAGA':
+        return 'var(--color-paga)';
+      case 'FINALIZADA':
+        return 'var(--color-finalizada)';
+      default:
+        return 'gray';
+    }
+  }
+
 }
--- frontend/src/app/pages/dashboard-funcionario/dashboard-funcionario.component.ts
+++ frontend/src/app/pages/dashboard-funcionario/dashboard-funcionario.component.ts
@@ -1,22 +1,36 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { materialImports } from "../../material-imports";
 import { CardSolicitacaoComponent } from "../../components/cards/card-solicitacao/card-solicitacao.component";
 import { NavbarComponent } from "../../components/navbar/navbar.component";
-import { TelaFuncionariosComponent } from "../tela-funcionarios/tela-funcionarios.component";
-import { CardFuncionarioComponent } from "../../components/cards/card-funcionario/card-funcionario.component";
-import { CardEquipamentoComponent } from "../../components/cards/card-equipamento/card-equipamento.component";
+import { Solicitacao } from "../../models/Solicitacao.model";
+import { CommonModule } from "@angular/common";
+
 
 @Component({
   selector: "app-dashboard-funcionario",
   imports: [
+    CommonModule,
     ...materialImports,
     CardSolicitacaoComponent,
-    NavbarComponent,
-    TelaFuncionariosComponent,
-    CardFuncionarioComponent,
-    CardEquipamentoComponent,
+    NavbarComponent
   ],
   templateUrl: "./dashboard-funcionario.component.html",
-  styleUrl: "./dashboard-funcionario.component.css",
+  styleUrl: "./dashboard-funcionario.component.css"
 })
-export class DashboardFuncionarioComponent {}
+export class DashboardFuncionarioComponent implements OnInit{
+  solicitacoes: Solicitacao[] = [];
+
+  ngOnInit(): void {
+      this.solicitacoes = [
+        { id: 101, data: '2025-04-05', hora: '14:30', cliente: 'Maria Joaquina', equipamento: 'Notebook Dell', estado: 'ABERTA' },
+        { id: 102, data: '2025-04-02', hora: '15:15', cliente: 'Maria Joaquina', equipamento: 'Impressora Xerox', estado: 'ORÇADA' },
+        { id: 103, data: '2025-03-17', hora: '16:45', cliente: 'Maria Joaquina', equipamento: 'Mouse de bolinha', estado: 'REJEITADA' },
+        { id: 104, data: '2025-04-04', hora: '17:20', cliente: 'Maria Joaquina', equipamento: 'Teclado com farelo de pão', estado: 'APROVADA' },
+        { id: 105, data: '2025-02-10', hora: '08:40', cliente: 'Maria Joaquina', equipamento: 'Notebook Positivo feito pra defeito', estado: 'REDIRECIONADA' },
+        { id: 106, data: '2025-04-06', hora: '09:15', cliente: 'Maria Joaquina', equipamento: 'Impressora HP (HarryPotter)', estado: 'ARRUMADA' },
+        { id: 107, data: '2025-02-10', hora: '08:45', cliente: 'Maria Joaquina', equipamento: 'Monitor Tubo', estado: 'PAGA' },
+        { id: 108, data: '2025-04-08', hora: '11:20', cliente: 'Maria Joaquina', equipamento: 'Monitor LG 24"', estado: 'FINALIZADA' }
+      ];
+      console.log('Solicitações carregadas:', this.solicitacoes);
+  }
+}
