a0a2e65a348b157128d84536920bcf45543c2dc5
lvtrindade <lucasventurintrindade@gmail.com>
2025-04-22 14:01:37 -0300

    feat(elements): Tela de solicitar manutenção concluída

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -47,7 +47,7 @@ export const routes: Routes = [
   },
   
   {
-    Path: "abrir-solicitacao",
+    path: "abrir-solicitacao",
     component: TelaSolicitarManutencaoComponent,
   }
 ];
--- frontend/src/app/components/tela-solicitar-manutencao/tela-solicitar-manutencao.component.ts
+++ frontend/src/app/components/tela-solicitar-manutencao/tela-solicitar-manutencao.component.ts
@@ -1,8 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatOption, MatSelect } from '@angular/material/select';
 
 @Component({
   selector: 'app-tela-solicitar-manutencao',
-  imports: [],
+  imports: [CommonModule, MatButtonModule, NavbarComponent, MatFormField, MatLabel, MatInputModule, MatSelect, MatOption],
   templateUrl: './tela-solicitar-manutencao.component.html',
   styleUrl: './tela-solicitar-manutencao.component.css'
 })
