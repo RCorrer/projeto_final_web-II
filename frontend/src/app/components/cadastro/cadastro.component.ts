7618fc60231b1b29ab9d20cd77d8fa6dcdae3b28
lvtrindade <lucasventurintrindade@gmail.com>
2025-05-09 17:10:23 -0300

    style(elements): Teste estilização cadastro

--- frontend/src/app/components/cadastro/cadastro.component.ts
+++ frontend/src/app/components/cadastro/cadastro.component.ts
@@ -1,15 +1,46 @@
-import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { RouterLink } from '@angular/router';
+import { Component, AfterViewInit } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { RouterLink } from "@angular/router";
 
 @Component({
-  selector: 'app-cadastro',
+  selector: "app-cadastro",
   imports: [MatFormFieldModule, MatInputModule, MatButtonModule, RouterLink],
-  templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.css'
+  templateUrl: "./cadastro.component.html",
+  styleUrl: "./cadastro.component.css",
 })
-export class CadastroComponent {
+export class CadastroComponent implements AfterViewInit {
+  private audio = new Audio("bipbip-sound.mp3");
 
+  ngAfterViewInit() {
+    const interBubble = document.querySelector(".interactive") as HTMLElement;
+    let curX = 0;
+    let curY = 0;
+    let tgX = 0;
+    let tgY = 0;
+
+    function move() {
+      curX += (tgX - curX) / 20;
+      curY += (tgY - curY) / 20;
+      interBubble.style.transform = `translate(${Math.round(
+        curX
+      )}px, ${Math.round(curY)}px)`;
+      requestAnimationFrame(() => {
+        move();
+      });
+    }
+
+    window.addEventListener("mousemove", (event) => {
+      tgX = event.clientX;
+      tgY = event.clientY;
+    });
+
+    move();
+  }
+
+  playSound() {
+    this.audio.currentTime = 0;
+    this.audio.play().catch((e) => console.error("Audio playback failed:", e));
+  }
 }
