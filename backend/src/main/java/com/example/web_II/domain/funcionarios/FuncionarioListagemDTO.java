3a897cee7277f002ea257690cbf22fe5610300d6
JoaoWalter193 <joaowalter193@gmail.com>
2025-06-13 12:43:27 -0300

    id no cliente

--- backend/src/main/java/com/example/web_II/domain/funcionarios/FuncionarioListagemDTO.java
+++ backend/src/main/java/com/example/web_II/domain/funcionarios/FuncionarioListagemDTO.java
@@ -3,12 +3,14 @@ package com.example.web_II.domain.funcionarios;
 import java.time.LocalDate;
 
 public record FuncionarioListagemDTO(
+        String id,
         String nome,
         String email,
         LocalDate nascimento
 ) {
     public FuncionarioListagemDTO(Funcionario funcionario) {
         this(
+                funcionario.getUsuario() != null ? funcionario.getUsuario().getId() : null,
                 funcionario.getUsuario() != null ? funcionario.getUsuario().getNome() : null,
                 funcionario.getUsuario() != null ? funcionario.getUsuario().getEmail() : null,
                 funcionario.getNascimento()
